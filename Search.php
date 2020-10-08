8ae367a82a0ece1a91ff28b595cd8eb7f7be23d0
lucius02 <54938206+lucius02@users.noreply.github.com>
2020-10-08 14:07:08 +0200

    update search

--- Search.php
+++ Search.php
@@ -1,32 +1,41 @@
+<?php
+include ("ini/connect.php");
+
+$con or die("could not connect");
+$output ='';
+print_r($_POST);
+
+//collect
+if (isset($_POST['search'])){
+    $searchq = $_POST['search'];
+    $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
+	$query = pg_query("SELECT * FROM sch_map.kenniskaart WHERE onderwerp LIKE '%$search%' #OR SurName LIKE '%$searchq%'") or die("could not search");
+    $count = pg_num_rows($query);
+    if($count == 0){
+        $output = 'There was no search results!';
+    }else{
+        while($row = pg_fetch_array($query)){
+            $onderwerp = $row['onderwerp'];
+            //$lname = $row['SurName'];
+            //$id = $row['id'];
+
+			$output .= '<div>'.$onderwerp.#''.$lname.
+			'</div>';
+        }
+    }
+}
+?>
+<!DOCTYPE html>
 <html>
 <head>
-	<title>Search</title>
+    <title>Search</title>
 </head>
-
-<div align="center">
-
-<?php
-	// include ('includes/header.html')
-?>
-
 <body>
-<p>&nbsp </p>
-<p>Enter a onderwerp to begin searching</p>
-<p>&nbsp </p>
-
-<form name="form" action="searchresult.php" method="post">
-<input type="text" name="$_POST['onderwerp']" />
-<input type="submit" name="Submit" value="Search" />
-</form>
-
-<p></p>
-<p>&nbsp </p>
+    <form method="post" action="searchresult.php">
+        <input type="text" name="search" placeholder="Zoeken voor Onderwerpen">
+		<input type="submit" value="Submit">
+	</form>
 </body>
-
-	<?php
-	//include ('includes/footer.html')
-?>
-
-</div>
-
 </html>
+
+<?php print("$output");?>
--- searchresult.php
+++ searchresult.php
@@ -1,116 +1,34 @@
-<html>
-
-<head>
-<title>Search</title>
-</head>
-
-<?php
-	// include ('includes/header.html');
-?>	
-
-<div align="center">
-
-<body>
-
-<p>&nbsp</p>
 
 
-<?php
-//connecting to the database
-include("ini/connect.php");
-?> 
-
-<?php
-
-//$sql = "select * from jfs where author ilike '%$search_author%' order by issue";
-
-//$sql = "select* from jfs where author like '%searchstring%' order by issue";
-
-$sql = "select * from sch_map.kenniskaart where onderwerp like '%$search%'" ; 
-
-            $result_set = pg_Exec ($con, $sql);
-            $rows = pg_NumRows($result_set);
-
-            if ((!$result_set) || ($rows < 1)) {
-                  echo "<H1>ERROR - no rows returned</H1><P>";
-                  exit;  //exit the script
-                  }
-
-            for ($j=0; $j < $rows; $j++)
-               {
-               $onderwerp = pg_result($result_set, $j, "onderwerp");
-               $rol = pg_result($result_set, $j, "rol");
-               $competentie = pg_result($result_set, $j, "competentie");
-               $wat = pg_result($result_set, $j, "wat");
-               $why = pg_result($result_set, $j, "why");
-               $how = pg_result($result_set, $j, "how");
-               $plaatje = pg_result($result_set, $j, "plaatje");
-               $bronnen = pg_result($result_set, $j, "bronnen");
-               $niveau = pg_result($result_set, $j, "niveau");
-               $studieduur = pg_result($result_set, $j, "studieduur");
-               $rating = pg_result($result_set, $j, "rating");
-
-
-		?> 
-		<table table border="3" width=60%>
-		<tr><th>Issue</th><th>Page</th><th>Author</th><th>Title</th><th>Format</th><th>URL</th></tr>
-              <TR>
-              <TD width=10%> 
-			<?php echo "$onderwerp"; ?>
-              </TD>
-              <TD width=10%>
-			<?php echo "$rol"; ?>
-              </TD>
-              <TD width=15%>
-			<?php echo "$competentie"; ?>
-              </TD>
-              <TD>
-			<?php echo "$wat"; ?>
-              </TD>
-              <TD width=10%>
-			<?php echo "$why"; ?>
-              </TD>
-		      <TD width=30%>
-              <?php echo "$how"; ?>
-              </TD>
-              <TD width=10%>
-			<?php echo "$plaatje"; ?>
-              </TD>
-              <TD width=10%>
-			<?php echo "$bronnen"; ?>
-              </TD>
-              <TD width=10%>
-			<?php echo "$niveau"; ?>
-              </TD>
-              <TD width=10%>
-			<?php echo "$studieduur"; ?>
-              </TD>
-              <TD width=10%>
-			<?php echo "$rating"; ?>
-              </TD>
-              </TR>
-		  
-		  </table>
-
-<?php		
-
-               }
-?>
-
-
-<?php
-               pg_FreeResult($result_set);
-               pg_Close($con);
-?> 
-</TABLE>	
-
-</body>
-
+<div class="row">
+    <h3>List of Student Volunteers</h3>
 </div>
+<div class="row">
+    <table class="table table-striped table-bordered">
+    <thead>
+        <tr>
+            <td>Onderwerp</td>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+        include ("ini/connect.php");
 
-
-<?php
-//include ('includes\footer.html');
-?>	
-
-</html>
+        $sql = "SELECT * FROM sch_map.kenniskaart WHERE onderwerp LIKE '%$search%' #OR LastName LIKE '%$searchq%' ORDER BY id DESC";
+        $result = pg_query($con,$sql);
+        $row = pg_fetch_array($result);
+        
+        foreach ($pdo->query($sql) as $row) {
+            echo '<tr>';
+            echo '<td>'. $row['onderwerp'] . '</td>';
+            echo '<td width=250>';
+            echo '<a class="btn" href="read.php?id='.$row['id'].'">Read</a>';
+            echo ' ';
+            echo '<a class="btn" href="update.php?id='.$row['id'].'">Update</a>';
+            echo ' ';
+            echo '<a class="btn" href="delete.php?id='.$row['id'].'">Delete</a>';
+            echo '</td>';
+            echo '</tr>';
+        }
+        $con::disconnect();
+    ?>
