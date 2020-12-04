de1cbd9cb08ea35add6eee7f611f8e8abe354736
lucius02 <54938206+lucius02@users.noreply.github.com>
2020-12-04 13:56:05 +0100

    update API

--- dataUpdate.php
+++ dataUpdate.php
@@ -47,7 +47,6 @@
 
             $sql = "UPDATE sch_map.kenniskaart ". "SET wat = $wat, why = $why, how = $how, rol = $rol, niveau = $niveau, competentie = $competentie, studieduur = $studieduur, rating = $rating, bronnen = $bronnen ". 
                "WHERE onderwerp = $onderwerp" ;
-            pg_select ($connection, $table_name, array ($assoc_array, $options = PGSQL_DML_EXEC, $result_type = PGSQL_ASSOC));
             $retval = pg_query( $sql, $conn );
             
             if(! $retval ) {
@@ -58,6 +57,7 @@
             pg_close($conn);
          }else {
             ?>
+
                <form action="dataUpdate.php" enctype="multipart/form-data" action = "dataUpdate.php">
                   <table width = "400" cellspacing = "1" 
                      cellpadding = "2">
@@ -86,92 +86,98 @@
                            id = "how"></td>
                      </tr>
                      <tr>
-                        <label class="label"for = "niveau">Niveau:</label><br>
-                        <div class="niveau_block" required>
-                            <input class="invulbox" type = "checkbox" id="niveau1" name = "niveau[]" value="Beginner">
-                            <label class="checkbox"for ="niveau1">Beginner</label><br>
-                            <input class="invulbox" type = "checkbox" id="niveau2" name = "niveau[]" value="Gevorderde">
-                            <label class="checkbox"for ="niveau2">Gevorderde</label><br>
-                            <input class="invulbox" type = "checkbox" id="niveau3" name = "niveau[]" value="Expert">
-                            <label class="checkbox"for ="niveau3">Expert</label><br>
-                            <script type="text/javascript">
-                                $('.invulbox').on('change', function() {
-                                    $('.invulbox').not(this).prop('checked', false);  
-                                });
-                            </script>
-                        </div>
+                        <td>
+                           <label class="label"for="rol">Niveau:</label><br>
+                           <div class="niveau_block" required>
+                              <input class="invulbox" type = "checkbox" id="niveau1" name = "niveau[]" value="Beginner">
+                              <label class="checkbox"for ="niveau1">Beginner</label><br>
+                              <input class="invulbox" type = "checkbox" id="niveau2" name = "niveau[]" value="Gevorderde">
+                              <label class="checkbox"for ="niveau2">Gevorderde</label><br>
+                              <input class="invulbox" type = "checkbox" id="niveau3" name = "niveau[]" value="Expert">
+                              <label class="checkbox"for ="niveau3">Expert</label><br>
+                              <script type="text/javascript">
+                                 $('.invulbox').on('change', function() {
+                                       $('.invulbox').not(this).prop('checked', false);  
+                                 });
+                              </script>
+                           </div>
+                        </td>
                      </tr>
                      <tr>
-                        <label class="label"for = "rol">Rol:</label><br>
-                        <div class="check_block" required>
-                            <input class="invulbox2" type = "checkbox" id="rol1" name = "rol[]" value="FE">
-                            <label class="checkbox2"for ="niveau1">FE</label><br>
-                            <input class="invulbox2" type = "checkbox" id="rol2" name = "rol[]" value="BE">
-                            <label class="checkbox2"for ="niveau2">BE</label><br>
-                            <input class="invulbox2" type = "checkbox" id="rol3" name = "rol[]" value="AI">
-                            <label class="checkbox2"for ="niveau3">AI</label><br>
-                            <input class="invulbox2" type = "checkbox" id="rol4" name = "rol[]" value="PO">
-                            <label class="checkbox2"for ="niveau1">PO</label><br>
-                            <input class="invulbox2" type = "checkbox" id="rol5" name = "rol[]" value="CSC">
-                            <label class="checkbox2"for ="niveau2">CSC</label><br>
-                            <input class="invulbox2" type = "checkbox" id="rol6" name = "rol[]" value="UX Designer">
-                            <label class="checkbox2"for ="niveau3">UX Designer</label><br>
-                        </div>
+                        <td>
+                           <label class="label"for = "rol">Rol:</label><br>
+                           <div class="check_block" required>
+                              <input class="invulbox2" type = "checkbox" id="rol1" name = "rol[]" value="FE">
+                              <label class="checkbox2"for ="niveau1">FE</label><br>
+                              <input class="invulbox2" type = "checkbox" id="rol2" name = "rol[]" value="BE">
+                              <label class="checkbox2"for ="niveau2">BE</label><br>
+                              <input class="invulbox2" type = "checkbox" id="rol3" name = "rol[]" value="AI">
+                              <label class="checkbox2"for ="niveau3">AI</label><br>
+                              <input class="invulbox2" type = "checkbox" id="rol4" name = "rol[]" value="PO">
+                              <label class="checkbox2"for ="niveau1">PO</label><br>
+                              <input class="invulbox2" type = "checkbox" id="rol5" name = "rol[]" value="CSC">
+                              <label class="checkbox2"for ="niveau2">CSC</label><br>
+                              <input class="invulbox2" type = "checkbox" id="rol6" name = "rol[]" value="UX Designer">
+                              <label class="checkbox2"for ="niveau3">UX Designer</label><br>
+                           </div>
+                        </td>
                      </tr>
                      <tr>
-                        <label class="label"for = "competentie">Competentie:</label><br>
-                        <div class="ow_block" required>
-                            <input class="invulbox3" type = "checkbox" id="competentie1" name="competentie[]" value="Gebruikersinteractie Analyseren">
-                            <label class="checkbox3" for ="competentie1">Gebruikersinteractie Analyseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie2" name="competentie[]" value="Gebruikersinteractie Adviseren">
-                            <label class="checkbox3" for ="competentie2">Gebruikersinteractie Adviseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie3" name="competentie[]" value="Gebruikersinteractie Ontwerpen">
-                            <label class="checkbox3" for ="competentie3">Gebruikersinteractie Ontwerpen</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie4" name="competentie[]" value="Gebruikersinteractie Realiseren">
-                            <label class="checkbox3" for ="competentie4">Gebruikersinteractie Realiseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie5" name="competentie[]" value="Gebruikersinteractie Manage & control">
-                            <label class="checkbox3" for ="competentie5">Gebruikersinteractie Manage & control</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie" name="competentie[]" value="Organisatieprocessen Analyseren">
-                            <label class="checkbox3" for ="competentie6">Organisatieprocessen Analyseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie7" name="competentie[]" value="Organisatieprocessen Adviseren">
-                            <label class="checkbox3" for ="competentie7">Organisatieprocessen Adviseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie8" name="competentie[]" value="Organisatieprocessen Ontwerpen">
-                            <label class="checkbox3" for ="competentie8">Organisatieprocessen Ontwerpen</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie9" name="competentie[]" value="Organisatieprocessen Realiseren">
-                            <label class="checkbox3" for ="competentie9">Organisatieprocessen Realiseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie10" name="competentie[]" value="Organisatieprocessen Manage & control">
-                            <label class="checkbox3" for ="competentie10">Organisatieprocessen Manage & control</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie11" name="competentie[]" value="Infrastructuur Analyseren">
-                            <label class="checkbox3" for ="competentie11">Infrastructuur Analyseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie12" name="competentie[]" value="Infrastructuur Adviseren">
-                            <label class="checkbox3" for ="competentie12">Infrastructuur Adviseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie13" name="competentie[]" value="Infrastructuur Ontwerpen">
-                            <label class="checkbox3" for ="competentie13">Infrastructuur Ontwerpen</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie14" name="competentie[]" value="Infrastructuur Realiseren">
-                            <label class="checkbox3" for ="competentie14">Infrastructuur Realiseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie15" name="competentie[]" value="Infrastructuur Manage & control">
-                            <label class="checkbox3" for ="competentie15">Infrastructuur Manage & control</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie16" name="competentie[]" value="Software Analyseren">
-                            <label class="checkbox3" for ="competentie16">Software Analyseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie17" name="competentie[]" value="Software Adviseren">
-                            <label class="checkbox3" for ="competentie17">Software Adviseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie18" name="competentie[]" value="Software Ontwerpen">
-                            <label class="checkbox3" for ="competentie18">Software Ontwerpen</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie19" name="competentie[]" value="Software Realiseren">
-                            <label class="checkbox3" for ="competentie19">Software Realiseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie20" name="competentie[]" value="Software Manage & control">
-                            <label class="checkbox3" for ="competentie20">Software Manage & control</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie21" name="competentie[]" value="Hardware interfacing Analyseren">
-                            <label class="checkbox3" for ="competentie21">Hardware interfacing Analyseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie22" name="competentie[]" value="Hardware interfacing Adviseren">
-                            <label class="checkbox3" for ="competentie22">Hardware interfacing Adviseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie23" name="competentie[]" value="Hardware interfacing Ontwerpen">
-                            <label class="checkbox3" for ="competentie23">Hardware interfacing Ontwerpen</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie24" name="competentie[]" value="Hardware interfacing Realiseren">
-                            <label class="checkbox3" for ="competentie24">Hardware interfacing Realiseren</label><br>
-                            <input class="invulbox3" type = "checkbox" id="competentie25" name="competentie[]" value="Hardware interfacing Manage & control">
-                            <label class="checkbox3" for ="competentie25">Hardware interfacing Manage & control</label><br>
-                        </div>
+                        <td>
+                           <label class="label"for = "competentie">Competentie:</label><br>
+                           <div class="ow_block" required>
+                              <input class="invulbox3" type = "checkbox" id="competentie1" name="competentie[]" value="Gebruikersinteractie Analyseren">
+                              <label class="checkbox3" for ="competentie1">Gebruikersinteractie Analyseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie2" name="competentie[]" value="Gebruikersinteractie Adviseren">
+                              <label class="checkbox3" for ="competentie2">Gebruikersinteractie Adviseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie3" name="competentie[]" value="Gebruikersinteractie Ontwerpen">
+                              <label class="checkbox3" for ="competentie3">Gebruikersinteractie Ontwerpen</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie4" name="competentie[]" value="Gebruikersinteractie Realiseren">
+                              <label class="checkbox3" for ="competentie4">Gebruikersinteractie Realiseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie5" name="competentie[]" value="Gebruikersinteractie Manage & control">
+                              <label class="checkbox3" for ="competentie5">Gebruikersinteractie Manage & control</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie" name="competentie[]" value="Organisatieprocessen Analyseren">
+                              <label class="checkbox3" for ="competentie6">Organisatieprocessen Analyseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie7" name="competentie[]" value="Organisatieprocessen Adviseren">
+                              <label class="checkbox3" for ="competentie7">Organisatieprocessen Adviseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie8" name="competentie[]" value="Organisatieprocessen Ontwerpen">
+                              <label class="checkbox3" for ="competentie8">Organisatieprocessen Ontwerpen</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie9" name="competentie[]" value="Organisatieprocessen Realiseren">
+                              <label class="checkbox3" for ="competentie9">Organisatieprocessen Realiseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie10" name="competentie[]" value="Organisatieprocessen Manage & control">
+                              <label class="checkbox3" for ="competentie10">Organisatieprocessen Manage & control</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie11" name="competentie[]" value="Infrastructuur Analyseren">
+                              <label class="checkbox3" for ="competentie11">Infrastructuur Analyseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie12" name="competentie[]" value="Infrastructuur Adviseren">
+                              <label class="checkbox3" for ="competentie12">Infrastructuur Adviseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie13" name="competentie[]" value="Infrastructuur Ontwerpen">
+                              <label class="checkbox3" for ="competentie13">Infrastructuur Ontwerpen</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie14" name="competentie[]" value="Infrastructuur Realiseren">
+                              <label class="checkbox3" for ="competentie14">Infrastructuur Realiseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie15" name="competentie[]" value="Infrastructuur Manage & control">
+                              <label class="checkbox3" for ="competentie15">Infrastructuur Manage & control</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie16" name="competentie[]" value="Software Analyseren">
+                              <label class="checkbox3" for ="competentie16">Software Analyseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie17" name="competentie[]" value="Software Adviseren">
+                              <label class="checkbox3" for ="competentie17">Software Adviseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie18" name="competentie[]" value="Software Ontwerpen">
+                              <label class="checkbox3" for ="competentie18">Software Ontwerpen</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie19" name="competentie[]" value="Software Realiseren">
+                              <label class="checkbox3" for ="competentie19">Software Realiseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie20" name="competentie[]" value="Software Manage & control">
+                              <label class="checkbox3" for ="competentie20">Software Manage & control</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie21" name="competentie[]" value="Hardware interfacing Analyseren">
+                              <label class="checkbox3" for ="competentie21">Hardware interfacing Analyseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie22" name="competentie[]" value="Hardware interfacing Adviseren">
+                              <label class="checkbox3" for ="competentie22">Hardware interfacing Adviseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie23" name="competentie[]" value="Hardware interfacing Ontwerpen">
+                              <label class="checkbox3" for ="competentie23">Hardware interfacing Ontwerpen</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie24" name="competentie[]" value="Hardware interfacing Realiseren">
+                              <label class="checkbox3" for ="competentie24">Hardware interfacing Realiseren</label><br>
+                              <input class="invulbox3" type = "checkbox" id="competentie25" name="competentie[]" value="Hardware interfacing Manage & control">
+                              <label class="checkbox3" for ="competentie25">Hardware interfacing Manage & control</label><br>
+                           </div>
+                        </td>
                      </tr>
                      <tr>
                         <td width = "100">studieduur</td>
